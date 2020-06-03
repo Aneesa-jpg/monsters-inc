23034eaff7a2a909ddd76f9954dede6130dba9e9
unknown <aneesa.farook.sait@gmail.com>
2020-06-03 11:24:05 +0530

    Updates

---
+++ precache-manifest.bf2fd07458f1b2de5e8b094931e68c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e249e3ac263638090565e224f00b5c",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "58b076b047283e676370",
+    "url": "/monsters-inc/static/css/main.2ebd9658.chunk.css"
+  },
+  {
+    "revision": "44c9345a90a3490cd22d",
+    "url": "/monsters-inc/static/js/2.d2a7826c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-inc/static/js/2.d2a7826c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b076b047283e676370",
+    "url": "/monsters-inc/static/js/main.385dadbf.chunk.js"
+  },
+  {
+    "revision": "65fd8189885c3c5aa410",
+    "url": "/monsters-inc/static/js/runtime-main.932907bf.js"
+  }
+]);
